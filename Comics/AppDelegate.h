0c4d1ab97717bac3bd4983334e06737039b7af56
Jason <8876142@qq.com>
2018-05-30 12:33:41 +0800

    Initial Commit

---
+++ Comics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Comics
+//
+//  Created by Jason on 2018/5/30.
+//  Copyright © 2018年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Comics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Comics
+//
+//  Created by Jason on 2018/5/30.
+//  Copyright © 2018年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
